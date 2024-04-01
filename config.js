9175cd2b822ff8436ff97874276bd7d4eb18587f
Metin Çelik <metincelik@Metin-MacBook-Pro.local>
2024-04-01 23:39:45 +0300

    .

---
+++ config.js
@@ -0,0 +1,3 @@
+export const GOOGLE_CLOUD_PROJECT_ID = process.env.GOOGLE_CLOUD_PROJECT_ID;    
+export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;  
+export const GOOGLE_CLOUD_REGION = process.env.GOOGLE_CLOUD_REGION;
