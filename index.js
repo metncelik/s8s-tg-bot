a81be0cfbcdabbf91cb1bf76ef2393047318160b
Metin Çelik <metincelik@Metin-MacBook-Pro.local>
2024-04-02 01:10:16 +0300

    .

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import { Telegraf } from 'telegraf';
-import { GOOGLE_CLOUD_PROJECT_ID, TELEGRAM_BOT_TOKEN, GOOGLE_CLOUD_REGION, FUNCTION_NAME  } from './config.js'; 
+import { GOOGLE_CLOUD_PROJECT_ID, TELEGRAM_BOT_TOKEN, GOOGLE_CLOUD_REGION, FUNCTION_NAME } from './config.js';
 
 const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
 bot.start((ctx) => ctx.reply(`Welcome to the most silly bot you'll ever see`));
 bot.help((ctx) => ctx.reply(`Write anything to me and I'll repeat it :)`));
-bot.on('text', (ctx) => ctx.reply(ctx.message.text)); //listen to every text message
-bot.on('message', ctx => ctx.reply('Command not recognized')); //avoid timeouts with unsupported commands
+bot.on('text', (ctx) => ctx.reply(ctx.message.text));
+bot.on('message', ctx => ctx.reply('Command not recognized'));
 bot.telegram.setWebhook(
     `https://${GOOGLE_CLOUD_REGION}-${GOOGLE_CLOUD_PROJECT_ID}.cloudfunctions.net/${FUNCTION_NAME}`
 );
 
-export const telegramBotWebhook = async (req, res) => {
-    await bot.handleUpdate(req.body, res);
+export const telegramBotWebhook = (req, res) => {
+    bot.handleUpdate(req.body, res);
 };
